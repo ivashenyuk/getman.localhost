73ba0660cfbbebfa43c00ec7e209e740ef679fee
Yurii Ivashenyuk <yura.ivash@gmail.com>
2017-12-06 20:26:48 +0200

    header and nav is ready

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Гетьмани України</title>
+</head>
+<body>
+  <div class="wrapper">
+    <header> 
+        <a href="/"><img src="/img/background.jpg" alt="header"></a>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="/">Головна</a></li>
+            <li><a href="#">Історія виникнення козацтва</a></li>
+        </ul>
+    </nav>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+  </div>
+</body>
+</html>
